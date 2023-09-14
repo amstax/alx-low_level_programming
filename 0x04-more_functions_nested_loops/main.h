e253b93bd918290ce87be45de925931f30c3aad6
amstax <famkarriem@gmail.com>
2023-09-14 07:23:52 -0700

    0-isdigit.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -13,4 +11,3 @@ void print_triangle(int size);
 int mul(int a, int b);
 void print_number(int n);
 
-#endif
